cffd5b75924286115908773d75c4b1e64217fb56
dlwnsqls33-stack <dlwnsqls33@gmail.com>
2026-08-27 11:06:44 +0900

    [Feat] Monster 클래스 수정, Ummmmmmm 객체추가

--- ConsoleProject1/ConsoleProject1/Monster.cs
+++ ConsoleProject1/ConsoleProject1/Monster.cs
@@ -3,4 +3,7 @@
 public class Monster
 {
     public int Health { get; set; }
+    public int Damage { get; set; }
+    public int Speed { get; set; }
+    
 }
---
+++ ConsoleProject1/ConsoleProject1/Ummmmmmm.cs
@@ -0,0 +1,12 @@
+﻿namespace ConsoleProject1;
+
+public class Ummmmmmm : Monster
+{
+
+    public Ummmmmmm()
+    {
+        Health = 20;
+        Damage = 20;
+        Speed = 20;
+    }
+}
